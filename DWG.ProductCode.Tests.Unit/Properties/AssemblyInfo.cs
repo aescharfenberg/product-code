736c2f7a29fa02e3523f4e0f5698afa13613f2dd
Allen E. Scharfenberg <allen@scharfenberg.co>
2017-08-16 15:15:25 -0500

    Target .NET Standard 1.2 instead of core directly for interop with .NET Framework proper.

---
+++ DWG.ProductCode.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CQUEST")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright © CQUEST 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3be1f724-afb5-42d7-9a15-2b3a85487c37")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- DWG.ProductCode/RegularExpressions.cs
+++ DWG.ProductCode/RegularExpressions.cs
@@ -6,15 +6,15 @@ namespace DWG.ProductCode
     {
         internal static class PriceLookup
         {
-            internal static readonly Regex InitialRegex = new Regex("^[0-9]{4,6}$", RegexOptions.Compiled);
+            internal static readonly Regex InitialRegex = new Regex("^[0-9]{4,6}$");
 
-            internal static readonly Regex Series3000Regex = new Regex("^(?<code>([0]|[9])?(3[0-9]{3}))$", RegexOptions.Compiled);
+            internal static readonly Regex Series3000Regex = new Regex("^(?<code>([0]|[9])?(3[0-9]{3}))$");
 
-            internal static readonly Regex Series4000Regex = new Regex("^(?<code>([0]|[9])?(4[0-9]{3}))$", RegexOptions.Compiled);
+            internal static readonly Regex Series4000Regex = new Regex("^(?<code>([0]|[9])?(4[0-9]{3}))$");
 
-            internal static readonly Regex Series83000Regex = new Regex("^(?<code>8(3[0-9]{3}))$", RegexOptions.Compiled);
+            internal static readonly Regex Series83000Regex = new Regex("^(?<code>8(3[0-9]{3}))$");
 
-            internal static readonly Regex Series84000Regex = new Regex("^(?<code>8(4[0-9]{3}))$", RegexOptions.Compiled);
+            internal static readonly Regex Series84000Regex = new Regex("^(?<code>8(4[0-9]{3}))$");
         }
     }
 }
